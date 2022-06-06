efa9bdd2a1066d990c39466f1445f4d3759b6986
alvarezmartin3639 <60674744+alvarezmartin3639@users.noreply.github.com>
2022-06-06 01:52:15 -0300

    Subiendo trabajo practico numero 3

--- TP3/Álvarez.Martín.2-C.TPFinal/Alvarez.Martin.2-C.TPFinal/VentanaEmergenteConInputBox/FormVentanaEmergenteConInputBox.Designer.cs
+++ TP3/Álvarez.Martín.2-C.TPFinal/Alvarez.Martin.2-C.TPFinal/VentanaEmergenteConInputBox/FormVentanaEmergenteConInputBox.Designer.cs
@@ -89,7 +89,6 @@ namespace VentanaEmergenteConInputBox
             this.Name = "frmVentanaEmergenteConInputBox";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Titulo";
-            this.Load += new System.EventHandler(this.frmVentanaEmergenteConInputBox_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
